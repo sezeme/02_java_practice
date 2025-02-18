5ff49f8b4066c3789df71fb55463434a800cc37f
sezeme <stellaa223@gmail.com>
2025-02-18 15:16:44 +0900

    1st practice

---
+++ src/main/java/com/sezeme/practice/level02_normal/Application1.java
@@ -0,0 +1,8 @@
+package com.sezeme.practice.level02_normal;
+
+public class Application1 {
+    public static void main(String[] args) {
+        char ch = 'a';
+        System.out.println("문자 a의 unicode : "+ (int)ch);
+    }
+}

c4aaa7b1910cba8ebaa120fdfdcdef9381207ce5
sezeme <stellaa223@gmail.com>
2025-02-26 18:59:02 +0900

    generic

---
+++ src/main/java/com/sezeme/chap12/generic/level01/normal/Application1.java
@@ -0,0 +1,24 @@
+package com.sezeme.chap12.generic.level01.normal;
+
+import java.util.Arrays;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        System.out.println("swap 전 : " + Arrays.toString(intArray));  // [1, 2, 3, 4, 5]
+        swap(intArray, 1, 3);
+        System.out.println("swap 후 : " + Arrays.toString(intArray));  // [1, 4, 3, 2, 5]
+
+        String[] strArray = {"A", "B", "C", "D"};
+        System.out.println("swap 전 : " + Arrays.toString(strArray));  // [A, B, C, D]
+        swap(strArray, 0, 2);
+        System.out.println("swap 후 : " + Arrays.toString(strArray));  // [C, B, A, D]
+    }
+
+    public static <T> void swap(T[] array, int a, int b) {
+        T tmp = array[a];
+        array[a] = array[b];
+        array[b] = tmp;
+    }
+}
